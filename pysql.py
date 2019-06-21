9a66ffb795edb3cde4e74ada2cdd3cf900995baa
Michael Le <you@example.com>
2019-06-21 12:45:15 -0500

    select customer from table

--- pysql.py
+++ pysql.py
@@ -1,1 +1,16 @@
 import pyodbc
+
+conn = pyodbc.connect(
+    'Driver={SQL Server};'
+    'Server=localhost;'
+    'Database=pltfloor;'
+    'Trusted_Connection=yes'
+)
+
+cursor = conn.cursor()
+
+cursor.execute(
+    'SELECT c.CustomerId, c.CustomerName FROM CUSTOMERS c')
+
+for row in cursor:
+    print(row)
